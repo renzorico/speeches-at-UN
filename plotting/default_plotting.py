8d083811dd2e544504556a4d170027061156226e
Renzo Rico <renzorico10@gmail.com>
2023-06-05 15:02:32 +0200

    Added requirements file, dataset and default graph

---
+++ plotting/default_plotting.py
@@ -0,0 +1,34 @@
+import plotly.express as px
+import streamlit as st
+
+
+# Static graph:
+
+df = px.data.gapminder()
+
+fig = px.scatter(
+    df.query("year==1997"),
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
+
+# Animated graph:
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+fig_2= px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
+           size="pop", color="continent", hover_name="country",
+           log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
+st.plotly_chart(fig_2, theme=None, use_container_width=True)
