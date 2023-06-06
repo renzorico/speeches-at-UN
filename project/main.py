359abae63046464d5cfee8ce038a88a9454ff127
Renzo Rico <renzorico10@gmail.com>
2023-06-06 14:27:13 +0200

    Working on Bertopic function

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
 from data import load_data
 from preprocessing import basic_cleaning, preproc
 from tfidf import tfidf_vec
-
+from bertopic_vec import bertopic
 data = load_data()
 
 df = data[:10]
@@ -10,5 +10,7 @@ df.loc[:, ['preprocessed']] = df['cleaned'].apply(preproc)
 df.loc[:, ['preprocessed']] = df['preprocessed'].apply(" ".join)
 breakpoint()
 results = df['preprocessed'].apply(tfidf_vec)
+model, topics, probs = bertopic(df['preprocessed'])
 
+print(model.get_topic_info())
 print(results)
