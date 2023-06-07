278b0515686f1b176b1d0662e1094539ac241458
Louis Declee <louisdeclee@gmail.com>
2023-06-07 15:34:45 +0200

    made wordclouds

--- project/bertopic_vec.py
+++
@@ -1,8 +0,0 @@
-from bertopic import BERTopic
-
-
-def bertopic(texts):
-    topic_model = BERTopic()
-    topics, probs = topic_model.fit_transform(texts)
-
-    return topic_model
---
+++ streamlit/words_cloud.py
@@ -0,0 +1,44 @@
+from wordcloud import WordCloud
+import streamlit as st
+
+@st.cache_data
+def generate_merged_word_cloud(data, stop_words):
+    wordcloud = WordCloud(max_words=100, stopwords=stop_words, colormap='YlOrBr', background_color='white').generate(data)
+    return wordcloud
+
+@st.cache_data
+def generate_specific_word_cloud(year, country, data_dict, stop_words):
+    wordcloud = WordCloud(max_words=100, stopwords=stop_words, colormap='YlOrBr', background_color='white').generate(data_dict[(year,country)])
+    return wordcloud
+
+@st.cache_data
+def display_wordcloud(data_dict, stop_words):
+    #  Select the years and country from a dropdown menu
+    years = sorted(list(set([key[0] for key in data_dict.keys()])))
+    selected_year = st.selectbox("Select a year", years + ['Select All'])
+
+    countries = sorted(list(set([str(key[1]) for key in data_dict.keys()])))
+    selected_country = st.selectbox("Select a country", countries + ['Select All'])
+
+    # Generate word cloud for the selected year and country
+    if selected_year == 'Select All' and selected_country == 'Select All':
+        # Generate word cloud for all years and countries
+        keys_to_merge = [pair for pair in data_dict.keys()]
+        merged_values = ' '.join(data_dict[key] for key in keys_to_merge)
+        wordcloud = generate_merged_word_cloud(merged_values)
+    elif selected_year == 'Select All':
+        # Gnerate word cloud for all years, specific country
+        keys_to_merge = set([(pair[0], selected_country) for pair in data_dict.keys()])
+        merged_values = ' '.join(data_dict[key] for key in keys_to_merge if key in data_dict.keys())
+        stop_words + [selected_country]
+        wordcloud = generate_merged_word_cloud(merged_values)
+    elif selected_country == 'Select All':
+        # Generate word cloud for specific year, all countries
+        keys_to_merge = set([(selected_year, pair[1]) for pair in data_dict.keys()])
+        merged_values = ' '.join(data_dict[key] for key in keys_to_merge if key in data_dict.keys())
+        wordcloud = generate_merged_word_cloud(merged_values)
+    else:
+        # Generate word cloud for specific year and country
+        wordcloud = generate_specific_word_cloud(selected_year, selected_country, data_dict, stop_words)
+
+    return wordcloud
