4b63008909ffac752c37485091204af7348e9cce
Louis Declee <louisdeclee@gmail.com>
2023-06-08 15:12:01 +0200

    fixed map, did historgrams

--- streamlit/pages/histplot.py
+++ streamlit/pages/histplot.py
@@ -1,6 +1,13 @@
-from data import load_count_topic_overtime, load_data
+import streamlit as st
+from data import load_data
+from topicplot import display_topics, select_topic_hist
 
 _ , doc_topics, data = load_data()
-count_topics_df = load_count_topic_overtime(doc_topics, data)
 
 def hist_main():
+    display_topics(doc_topics, data)
+    words_list = select_topic_hist(doc_topics, data)
+    st.write(words_list)
+
+
+hist_main()
--- streamlit/pages/map.py
+++ streamlit/pages/map.py
@@ -5,7 +5,7 @@ from plot_map import select_topic, plot_geo_features
 joined_gdf = load_geodata()
 
 def map_main():
-    topic = select_topic(joined_gdf)
-    plot_geo_features(topic, joined_gdf)
+    selected_topic = select_topic(joined_gdf)
+    plot_geo_features(selected_topic, joined_gdf)
 
 map_main()
