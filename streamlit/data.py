ef7d681a8a3e9c0fa7f6e61da89787ac33a053e9
Nikita Polovinkin <poloniki@ya.ru>
2023-06-15 00:34:48 +0200

    New structure

--- streamlit/data.py
+++ streamlit/data.py
@@ -95,7 +95,7 @@ def get_data_wordcloud():
 
 @st.cache_data()
 def load_umap():
-    print(os.listdir())
+    print(f'{os.listdir()}------------------------------------------------------------------------')
 
     df = pd.read_csv('streamlit/umap.csv')
     return df
