ccdf6d640b3ff386c2837595e874486fd7bf5604
Louis Declee <louisdeclee@gmail.com>
2023-06-15 16:01:31 +0200

    final touches

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -5,8 +5,6 @@ from streamlit_player import st_player
 def main():
     st.title("Welcome to the United Nations!")
 
-    st_player("https://www.youtube.com/watch?v=eN2jqTilLOM&t=12s")
-
     st.markdown("<h3 style='font-family: Arial; font-size: 20px; text-align: right; line-height: 1.5;'>Sponsored by Kevin Hart...</h3>", unsafe_allow_html=True)
     st.markdown("<p style='font-family: Times New Roman; font-size: 16px; text-align: right; line-height: 0;'>and yours truly Will Smith.</p>", unsafe_allow_html=True)
 
